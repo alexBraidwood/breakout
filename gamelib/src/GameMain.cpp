c1d39d3400d6d99f093ac93c3cba93f6cacc475e
Alex Braidwood <alexb.devel@gmail.com>
2017-02-15 19:35:30 -0500

    Initial Commit

---
+++ gamelib/src/GameMain.cpp
@@ -0,0 +1,15 @@
+//
+// Created by alex on 2/15/17.
+//
+
+#include "GameMain.h"
+
+using namespace core;
+
+void GameMain::start() {
+
+}
+
+void GameMain::quit() {
+
+}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <GameMain.h>
+
+int main()
+{
+    core::GameMain game;
+    game.start();
+    game.quit();
+    return 0;
+}
